c7882b55ca90b1dbe1f6ba25ce9434fe486a44ff
Frank R. Leotta III <leottaif@go.stockton.edu>
2024-02-10 13:04:32 -0500

    found issue, fork in the road solution needed

--- MathFuntions.py
+++ MathFuntions.py
@@ -18,7 +18,8 @@ import math
 #divide
 #is_even
 #is_odd
-
+#add_string_ascii_word # this adds 2 strings together based on their ascii values.  Its technically correct, and good for a laugh.
+#add_string_ascii_number
 
 
 
@@ -92,34 +93,13 @@ print(C)
 # print(my_list)  # Output: [1, 2, 10, 4, 5]
 
 
-list1 = [1, 2, 3, 4, 5]
-list2 = [1, 2, 3, 4, 5]
-list3 = []
-list3.append(list1[0] + list2[0])
-print("this is list3")
-print(list3)
 
 
 
 
-def add_string_ascii_letter(a,b):
-    """Summery: This function adds two strings together and returns the result.  The strings are first converted to their ascii values and then added together.  The result is then converted back to a string and returned.  This is just for fun, and is tecnically correct, and good for a laugh.
-    It give you a letter, not a number.
+#issue if = if b is greater than a, then it will be negative
 
-    Args:
-        a (string): The first string to be added.
-        b (string): The second string to be added.
-
-    Returns:
-        string: The sum of the two strings.
-    
-    """
-
-
-a = 'a'
-a.split("_")
-
-print (a)
+#now if handled the opposite... need a fork in the road
 
 
 def add_string_ascii_word(a,b):
@@ -149,6 +129,10 @@ def add_string_ascii_word(a,b):
     # print ("this is a")
     # print(a)
     numberleftover = len(a) - len(b)
+    # if numberleftover <= 0:
+    #     numberleftover = 
+    print("this is numberleftover")
+    print(numberleftover)
     #if numberleftover is less than or equal to 0, pass
     if numberleftover <= 0:
         pass
@@ -156,10 +140,8 @@ def add_string_ascii_word(a,b):
         #transfer every element  in a up to the lens(numberleftover) to c
         for i in range(abs(numberleftover)):
             c.append(a[i])
-
-            
-    # print("this is c")
-    # print(c)
+    print("this is c")
+    print(c)
     c.reverse()
     # print("this is c after reverse")
     # print(c)
@@ -194,9 +176,21 @@ def add_string_ascii_word(a,b):
         else:
             while new_a[i] > 122:
                 new_a[i] = new_a[i] - 122
+    
+    print("this is new_a")
+    print(new_a)
+    #now to loop around so that all elements are between 65 and 122
+    for i in range(len(new_a)):
+        if new_a[i] <= 65:
+            new_a[i] = new_a[i] - 122
+    print("this is new_a")
+    print(new_a)
 
-    # print("this is new_a")
-    # print(new_a)
+    #absolute value to get rid of negatives
+    for i in range(len(new_a)):
+        new_a[i] = abs(new_a[i])
+
+
     # if element is 91, add 1 to it
     for i in range(len(new_a)):
         if new_a[i] == 91: #if element is 91, add 1 to it
@@ -214,8 +208,8 @@ def add_string_ascii_word(a,b):
         if new_a[i] == 96:
             new_a[i] = new_a[i] + 5
 
-    # print("this is new_a")
-    # print(new_a)
+    print("this is new_a")
+    print(new_a)
     #this will ensure each element is between 65 and 122, a letter of the alphabet
     #convert each element back to a letter
     for i in range(len(new_a)):
@@ -226,29 +220,19 @@ def add_string_ascii_word(a,b):
     #convert new_a to a string
     new_a = ''.join(new_a)
     #add c to new_a
+    print("this is c")
+    print(c)
     c = ''.join(c)
     new_a = new_a + c
-    # test ="test"
-    # new_a = new_a + test
+    test ="test"
+    new_a = new_a + test
 
     #return new_a
     return new_a
 
 
 
- 
-# Psudo code:
-#  if  i <= 65 subtract 122 from i
-# i >=122 subtract 122 until i =< eless than 122
-
-# def ascii_conversion(string):
-#     """Summery: This function takes a string and converts it to its ascii values.  It then returns a list of the ascii values.
-    
-#     Args:
-#         string (string): The string to be converted to ascii values.
-
-#     Returns:
-#         list: A list of the ascii values of the string.
-    
-#     """
-#     return [ord(c) for c in string]
+A = "hello how are you doing today"
+b = "I am doing well thank you for asking "
+print(" this is the result of adding two sentences together based on their ascii values. ")
+print(add_string_ascii_word(A,b)) 
