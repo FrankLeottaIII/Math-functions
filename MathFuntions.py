00e21ad2d7cbe600188b17995534ba6cd0b05f49
Frank R. Leotta III <leottaif@go.stockton.edu>
2024-02-09 22:18:07 -0500

    add, added

---
+++ MathFuntions.py
@@ -0,0 +1,45 @@
+#Math funtions
+
+#Author: Frank Robert Leotta III
+#Date started: 2/9/2024
+
+#Discription: This is a repository of math functions that I have written for various projects.  
+            
+
+# Please use word wrap to see the code properly.
+
+#Imports
+import math
+
+#table of contents:
+#add
+#subtract
+#multiply
+#divide
+#is_even
+#is_odd
+
+
+
+
+
+
+
+####################################################################################################
+
+def add(a,b):
+    """Summery: This function adds two varibles together and returns the result.  The varibles must be the same type (int, float, or string).  If the varibles are strings, they will be concatinated together.  If the varibles are floats, they will be added together and the result will be a float.  If the varibles are ints, they will be added together and the result will be an int.
+
+    Note: you will need to convert the varibles to the same type before using this function if you want to add a string to a number.
+    
+    Args:
+        a (int): The first number to be added.
+        b (int): The second number to be added.
+    
+    Returns:
+        int: The sum of the two numbers.
+    
+    """
+    return a+b
+
+
